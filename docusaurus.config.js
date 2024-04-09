e0a637b271550e8030ee8d4b6d33812d3fc76aae
crexroth <crexroth@metrohealth.org>
2024-04-09 15:46:29 -0500

    Reverting changes due to error being on DNS side

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/Cosmiic_favicon-150x150.png',
 
   // Set the production url of your site here
-  url: 'https://cosmiic-inc.github.io',
+  url: 'https://docs.cosmiic.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
