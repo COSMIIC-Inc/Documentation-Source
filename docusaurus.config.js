8c0b3eaa93d198ff1e583ae3b2413cfe585f6f59
crexroth <crexroth@metrohealth.org>
2024-06-14 08:16:16 -0400

    Removing serve to docs.cosmiic.org to redirect from there to projectname.github.io

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/Cosmiic_favicon-150x150.png',
 
   // Set the production url of your site here
-  url: 'https://docs.cosmiic.org',
+  url: 'https://cosmiic-inc.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
